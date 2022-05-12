b8ac9c7510de1866a8299a1d9cee00f1d1173d3d
0andme <yu00mi97@gmail.com>
2022-05-12 10:46:34 +0900

    doc : edit readme file

--- ch2-5-null-undefined.js
+++ ch2-5-null-undefined.js
@@ -1,14 +1,15 @@
 "use strict";
 // strict mode일때 error
-// let str: string = undefined;
+let str = undefined;
 // void에는 undefined만 가능
-let str22 = undefined;
+let voidVal = undefined;
 // void에는 null은 불가
-// let str33: void = null;
+voidVal = null;
 // 아래는 error가 나지 않는 경우
 let nullVal = null;
 let undefinedVal = undefined;
 //  strict mode에서 error가 안 나게 하려면
 let strVal = undefined;
+//런타임에서 null의 타입은 object이다.
 console.log(typeof null); //출력결과 : object
 console.log(typeof undefined); //출력결과 : undefined
