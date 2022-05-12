cc673d2d2051b0bb77e73afb1e19bad45a2d3316
0andme <yu00mi97@gmail.com>
2022-05-12 14:02:23 +0900

    doc : add source file

---
+++ ch.2/sources/tuple.js
@@ -0,0 +1,11 @@
+"use strict";
+let x = [1, "and"];
+// 순서와 자료형 모두 맞아야 한다
+x = [1, "name"];
+// error : 순서와 자료형이 틀림
+// x = ["name", 1];
+// error: x는 길이가 2인 값인데 3번째 자리 값을 지정하려고 해서
+// x[2] = "name";
+const data = ["name", 1];
+// error: 마찬가지로 구조 분해 할당시 없는 값에 접근하려고 해서
+// const [data1, data2, data3] = data;
