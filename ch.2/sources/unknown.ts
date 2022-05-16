bbd9bfc0792a694ea542637a9ee0c021bf75bf0f
0andme <yu00mi97@gmail.com>
2022-05-16 21:05:19 +0900

    doc : add source code

---
+++ ch.2/sources/unknown.ts
@@ -0,0 +1,16 @@
+declare const maybe: unknown;
+
+//error:  unknown타입의 값을 number타입의 변수에넣을수 없음
+// const aNum: number = maybe;
+
+// 아래코드는 런타임일 때 타입가드가 된다.
+if (maybe === true) {
+  // if문을 만나면서
+  // 타입가드가 되면서 maybe의 타입이 ture가 됨
+  const aBool: boolean = maybe;
+}
+
+if (typeof maybe === "string") {
+  // 타입가드가 되면서 maybe의 타입이 string이 됨
+  const string: string = maybe;
+}
